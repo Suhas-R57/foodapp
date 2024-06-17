72fad535e8d1fbb2808701dbe85928b29c64836b
Suhas-R57 <suhasr944@gmail.com>
2024-06-17 20:50:51 +0530

    modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="footer-content">
             <div className="footer-content-left">
                 <img src={assets.logo} alt="" />
-                <P> t7yegy kiokjhvujveoiu kwjjorkf</P>
+                <p> t7yegy kiokjhvujveoiu kwjjorkf jhygfk jhvbjkdgdhjgujk gyfukgigy iyiudhkge riu uigfhuhwuyfe riehihgreyfiue gherygiur</p>
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
                     <img src={assets.twitter_icon} alt="" />
@@ -16,13 +16,26 @@ const Footer = () => {
                 </div>
             </div>
             <div className='footer-content-center'>
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
                 
             </div>
             <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+91 9854388466</li>
+                    <li>contact@foodexpress.com</li>
+                </ul>
 
             </div>
             
         </div>
+        <p className='footer-copyright'>Copyright 2024 @ Foodexpress.com -All Right Reserved</p>
 
       
     </div>
