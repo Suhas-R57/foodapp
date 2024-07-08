80e59584d21c7214b6c4cdba1d3607b2d5156211
Suhas-R57 <suhasr944@gmail.com>
2024-07-08 18:46:06 +0530

    modified

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -4,6 +4,22 @@ import fs from 'fs'
 
 //to add food item
 const addFood = async(req,res)=>{
+    let image_filename = `${req.file.filename}`;
+    const food = new foodmodel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.category,
+        image:image_filename
+    })
+    try {
+        await food.save();
+        res.json({success:true,message:"food added"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})
+        
+    }
 
 }
 
