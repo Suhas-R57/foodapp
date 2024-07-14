25da60d64df6a50b2e0a615c4cc12a1c8f752d57
Shubhoshish R <shubho0208@gmail.com>
2024-07-14 11:34:12 +0530

    conn

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,8 @@
 import mongoose, { connect } from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://fooddel:nhce2024@cluster0.jgsiyhk.mongodb.net/food-del').then(()=>console.log("Database connected"));
+    await mongoose.connect('mongodb+srv://fooddel:nhce2024@cluster0.jgsiyhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("Database connected"));
 
 }
+//'mongodb+srv://fooddel:nhce2024@cluster0.jgsiyhk.mongodb.net/food-del'
+//mongodb+srv://fooddel:<password>@cluster0.jgsiyhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
