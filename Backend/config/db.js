78c99096aafbba13c13733863744c36c75681014
Suhas R <156928649+SuhasR57@users.noreply.github.com>
2024-12-07 20:20:50 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose, { connect } from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://fooddel:nhce2024@cluster0.jgsiyhk.mongodb.net/food-del').then(()=>console.log("Database connected"));
+    await mongoose.connect('mongodb+srv://<username>:<password>@cluster0.jgsiyhk.mongodb.net/food-del').then(()=>console.log("Database connected"));
 
 }
